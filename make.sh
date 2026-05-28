c539968eb15b4fd169cd73b1b69ff704c45ec8c8
Alan Lima <alanlimanvc@gmail.com>
2026-05-28 19:39:36 -0300

    chore: improve make script

--- make.sh
+++ make.sh
@@ -1,1 +1,2 @@
-gcc cmd/main.c internal/net.c internal/tcp/server.c -Wall -Wextra -Werror -g -o bin/sprout
+mkdir -p bin
+gcc cmd/main.c internal/net.c internal/work.c internal/slice.c internal/tcp/server.c -Wall -Wextra -Werror -g -o bin/sprout
